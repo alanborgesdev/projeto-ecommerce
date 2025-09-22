9be48498d3170d7bcbd4efdacab2f7cc5c13d573
Alan Borges <alanborges05@gmail.com>
2025-09-21 21:55:37 -0300

    adiciona funcinalidade de salvar produto no carrinho

---
+++ js/carrinho.js
@@ -0,0 +1,61 @@
+// Objetivo 1 - quando clicar no botão de adicionar ao carrinho:
+//     - atualizar o contador
+//     - adicionar o produto no localStorage
+//     - atualizar a tabela HTML do carrinho
+
+const botoesAdicionarAoCarrinho = document.querySelectorAll('.adicionar-ao-carrinho');
+
+botoesAdicionarAoCarrinho.forEach((botao) => {
+    botao.addEventListener('click', (evento) => {
+        const elementoProduto = evento.target.closest('.produto');
+        const produtoId = elementoProduto.dataset.id;
+        const produtoNome = elementoProduto.querySelector('.nome').textContent;
+        const produtoImagem = elementoProduto.querySelector('img').getAttribute('src');
+        const produtoPreco = parseFloat(
+            elementoProduto
+                .querySelector('.preco')
+                .textContent.replace('R$ ', '')
+                .replace('.', '')
+                .replace(',', '.')
+        );
+
+        //buscar a lista de produtos do localStorage
+        const carrinho = obterProdutosDoCarrinho();
+        //testar se o produto já está no carrinho
+        const existeProduto = carrinho.find((produto) => produto.id === produtoId);
+        //se existe produto, incrementar a quantidade
+        if (existeProduto) {
+            existeProduto.quantidade += 1;
+        } else {
+            //se não existe, adicionar o produto com quantidade 1
+            const produto = {
+                id: produtoId,
+                nome: produtoNome,
+                imagem: produtoImagem,
+                preco: produtoPreco,
+                quantidade: 1,
+            };
+            carrinho.push(produto);
+        }
+        salvarProdutosNoCarrinho(carrinho);
+    });
+});
+
+function salvarProdutosNoCarrinho(carrinho) {
+    localStorage.setItem('carrinho', JSON.stringify(carrinho));
+}
+
+function obterProdutosDoCarrinho() {
+    const produtos = localStorage.getItem('carrinho');
+    return produtos ? JSON.parse(produtos) : [];
+}
+
+// Objetivo 2 - remover produtos do carrinho:
+//     - ouvir o botão de deletar
+//     - remover do localStorage
+//     - atualizar o DOM e o total
+
+// Objetivo 3 - atualizar valores do carrinho:
+//     - ouvir mudanças de quantidade
+//     - recalcular total individual
+// - recalcular total geral
