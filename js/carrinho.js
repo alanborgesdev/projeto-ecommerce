2856e0a429d177cc78d9a51b196f6105dabacb28
Alan Borges <alanborges05@gmail.com>
2025-09-23 18:37:17 -0300

    adicionaa funcionalidade que monta a tabela do carrinho

--- js/carrinho.js
+++ js/carrinho.js
@@ -39,6 +39,7 @@ botoesAdicionarAoCarrinho.forEach((botao) => {
         }
         salvarProdutosNoCarrinho(carrinho);
         atualizarContadorDoCarrinho();
+        renderizarTabelaDoCarrinho();
     });
 });
 
@@ -53,15 +54,50 @@ function obterProdutosDoCarrinho() {
 
 //passo 4 - atualizar o contador do carrinho de compras
 function atualizarContadorDoCarrinho() {
-    const carrinho = obterProdutosDoCarrinho();
+    const produtos = obterProdutosDoCarrinho();
     let total = 0;
 
-    carrinho.forEach((produto) => {
+    produtos.forEach((produto) => {
         total += produto.quantidade;
     });
-    
+
     document.getElementById('contador-carrinho').textContent = total;
 }
 
 atualizarContadorDoCarrinho();
 
+//passo 5 - renderizar a tabela do carrinho de compras
+function renderizarTabelaDoCarrinho() {
+    const produtos = obterProdutosDoCarrinho();
+    const corpoTabela = document.querySelector('#modal-1-content table tbody');
+    corpoTabela.innerHTML = ''; // Limpar tabela antes de renderizar
+
+    produtos.forEach((produto) => {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `<td class="td-produto">
+                                        <img
+                                            src="${produto.imagem}"
+                                            alt="${produto.nome}"
+                                        />
+                                    </td>
+                                    <td>${produto.nome}</td>
+                                    <td class="td-preco-unitario">R$ ${produto.preco
+                                        .toFixed(2)
+                                        .replace('.', ',')}</td>
+                                    <td class="td-quantidade">
+                                        <input type="number" value="${
+                                            produto.quantidade
+                                        }" min="1" />
+                                    </td>
+                                    <td class="td-preco-total">R$ ${(
+                                        produto.preco * produto.quantidade
+                                    )
+                                        .toFixed(2)
+                                        .replace('.', ',')}</td>
+                                    <td><button class="btn-remover" data-id="${
+                                        produto.id
+                                    }" id="deletar"></button></td>`;
+        corpoTabela.appendChild(tr);
+    });
+}
+renderizarTabelaDoCarrinho();
